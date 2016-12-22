e04538656bb3dd6302c15fda2da899aaab7f8664
Kyle <idynkydnk@gmail.com>
2016-12-22 13:11:37 -0500

    build tree method complete, but can't figure out how to test it

--- binary_tree.rb
+++ binary_tree.rb
@@ -10,64 +10,54 @@ class BinaryTree
     if @root.value.nil?
       @root.value = data[0]
     end
-    node = @root
     (data.length - 1).times do |i|
       new_node = Node.new
       new_node.value = data[i + 1]
-      if new_node.value < node.value
-        new_node.right = node
-        node.left = new_node
-      else
-        new_node.left = node
-        node.right = new_node
-      end
-        
-
-      puts i + 1
+      insert_node(new_node, @root) 
     end
   end
 
-  def old_build_tree(data)
-    if data.length == 1
-      @root.value = data[0]
-    else
-      middle = data.length / 2
-      lower_middle = middle - 1
-      node = @root
-      node.value = data[middle]
-      until lower_middle < 0 
-        node.left = Node.new
-        node.left.value = data[lower_middle]
-        node.left.parent = node
-        node = node.left 
-        lower_middle -= 1
+  def insert_node(new_node, root)
+    if new_node.value < root.value
+      if root.left.nil?
+        root.left = new_node
+        new_node.parent = root
+      else
+        insert_node(new_node, root.left)
       end
-      node = @root
-      until middle == (data.length - 1)
-        node.right = Node.new
-        node.right.value = data[middle + 1]
-        node.right.parent = node
-        node = node.right
-        middle += 1 
+    else 
+      if root.right.nil?
+        root.right = new_node
+        new_node.parent = root
+      else
+        insert_node(new_node, root.right)
       end
     end
-      
   end
 
   def to_s
     node = @root
-    until !node.left
-      node = node.left
-    end
-    while node.parent
+    if node.left
+      to_s_left(node.left)
+    else
       puts node.value
-      node = node.parent
     end
-    until !node.right
+
+
+  end
+
+  def to_s_left(node)
+    if node.left
+      to_s_left(node.left)
+    else
       puts node.value
-      node = node.right
     end
-    puts node.value
+
+
+  end
+
+  def to_s_right(node)
+
   end
 
 end
--- run.rb
+++ run.rb
@@ -3,3 +3,10 @@ require_relative "binary_tree.rb"
 x = BinaryTree.new
 a = [1, 2, 3, 4, 5]
 x.build_tree(a)
+x.to_s
+puts
+puts
+y = BinaryTree.new
+a = [2, 5, 3, 9, 1, 4]
+y.build_tree(a)
+y.to_s
